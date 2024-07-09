bd4fef3f998828ada220b52e022cc359f9032895
kzhao <kzhao5@nd.edu>
2024-07-09 16:17:41 +0000

    change run_transfer_attach.sh

--- run_transfer_attach.sh
+++ run_transfer_attach.sh
@@ -1,17 +1,34 @@
-# shellcheck disable=SC2006
-py_path=`which python`
+# # shellcheck disable=SC2006
+# py_path=`which python`
+
+# run() {
+#     number=$1
+#     shift
+#     for i in $(seq $number); do
+#       # shellcheck disable=SC2068
+#       $@
+#       $py_path transfer_based_attack.py
+#     done
+# }
+
+# # shellcheck disable=SC2046
+# # shellcheck disable=SC2006
+# #echo $epoch
+# run "$1"
+#!/bin/bash
+
+py_path=$(which python)
 
 run() {
     number=$1
-    shift
-    for i in $(seq $number); do
-      # shellcheck disable=SC2068
-      $@
-      $py_path transfer_based_attack.py
+    for i in $(seq 1 "$number"); do
+        $py_path transfer_based_attack.py
     done
 }
 
-# shellcheck disable=SC2046
-# shellcheck disable=SC2006
-#echo $epoch
-run "$1"
+if [ "$1" == "--number" ]; then
+    run "$2"
+else
+    echo "Usage: $0 --number <number_of_runs>"
+    exit 1
+fi
